d069cc7fc9eaafdda01e0064f5d774449cf1fbeb
Diego Saldonid <dsaldoni@ucsd.edu>
2021-06-23 09:45:08 -0700

    got it on first try!

--- student_store_backend/student-store-schema.sql
+++ student_store_backend/student-store-schema.sql
@@ -1,8 +1,18 @@
 CREATE TABLE users (
   id          SERIAL PRIMARY KEY,
+  NAME        TEXT NOT NULL,
   password    TEXT NOT NULL,
   username    TEXT NOT NULL UNIQUE,
   email       TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 1),
   is_admin    BOOLEAN NOT NULL DEFAULT FALSE,
   created_at  TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE products (
+  id           SERIAL PRIMARY KEY,
+  NAME         TEXT NOT NULL,
+  category     TEXT NOT NULL,
+  image        TEXT NOT NULL UNIQUE,
+  description  TEXT NOT NULL UNIQUE,
+  price        BIGINT NOT NULL
 );
